bf3f2c837e41d0145bd3dc22f9c505ace6f405cf
Nagy Gergo <nagy.gergo@3pixel.hu>
2020-02-22 15:30:59 +0100

    safety

--- lib/betonmylife/controller/user.ex
+++ lib/betonmylife/controller/user.ex
@@ -1,36 +1,17 @@
 defmodule Betonmylife.User do
   use Plug.Router
-
+  alias Betonmylife.UserStore
 
   plug(:match)
   plug(:dispatch)
 
   @content_type "application/json"
 
-  get "/" do
-    IO.puts(
-      UserDto.from_map(
-        '{
-  "isDeleted": false,
-  "email": "oridun@huk.ki",
-  "password": "3HR!$&Siuo",
-  "firstName": "Myrtle",
-  "lastName": "Gray",
-  "pictureUrl": "http://www.gravatar.com/avatar/f24faa2f11a7d48e9c70b9d4a242f51a"
-}'
-      )
-    )
-    #    Store.set('users', UserDto.from_map('{
-    #  "id": "2c12d538-8e7d-520a-987e-b096213ae83e",
-    #  "isDeleted": false,
-    #  "email": "oridun@huk.ki",
-    #  "password": "3HR!$&Siuo",
-    #  "firstName": "Myrtle",
-    #  "lastName": "Gray",
-    #  "pictureUrl": "http://www.gravatar.com/avatar/f24faa2f11a7d48e9c70b9d4a242f51a"
-    #}'))
-    #    IO.puts(Store.fetch('users'))
-    send_resp(conn, 200, "Success!")
+  post "/" do
+
+    newUser = UserDto.from_map(conn.body_params)
+    UserStore.add(newUser)
+    send_resp(conn, 200, Poison.encode!(newUser))
   end
 
   post "/" do
