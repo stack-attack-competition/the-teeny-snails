57460cb773f79248c57f1725a10a7334179dcf94
PumpkinSeed <qwer.kocka@gmail.com>
2020-02-22 19:11:53 +0100

    Remove router

--- lib/betonmylife/controller/endpoint.ex
+++ lib/betonmylife/controller/endpoint.ex
@@ -27,8 +27,7 @@ defmodule Betonmylife.Endpoint do
       Plug.Adapters.Cowboy.http(__MODULE__, [], config)
     end
   end
-
-  forward("/hello", to: Betonmylife.Router)
+  
   forward("/auth", to: Betonmylife.Auth)
   forward("/users", to: Betonmylife.User)
   forward("/bets", to: Betonmylife.Bet)
